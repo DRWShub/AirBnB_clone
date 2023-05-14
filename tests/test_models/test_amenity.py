2e510bcc47394ef77f1c9fa642db7805ef66edc7
Eric Godworks <ericgodworks@yahoo.com>
2023-05-14 14:20:58 -0700

    test amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
 """Testing Amenity class from models.amenity"""
+
+
 import unittest
-
-# import sys module
-import sys
-#tell interpreter where to look
-sys.path.insert(0,"..")
-
 from models.base_model import BaseModel
 from models.amenity import Amenity
+import os
+import sys
 
 
 class TestAmenity(unittest.TestCase):
-        """Unittest cases for Amenity"""
+    """Unittest cases for Amenity"""
+    def setUp(self):
+        self.amenity = Amenity()
 
-        def setUp(self):
-            self.amenity = Amenity()
+    def test_amenity_check_subclass(self):
+        self.assertTrue(issubclass(type(self.amenity), BaseModel))
 
-        def test_amenity_check_subclass(self):
-            self.assertTrue(issubclass(type(self.amenity), BaseModel))
+    def test_amenity_check_class_attr(self):
+        self.assertTrue(hasattr(self.amenity, "name"))
 
-        def test_amenity_check_class_attr(self):
-            self.assertTrue(hasattr(self.amenity, "name"))
-
-        def test_amenity_check_attr_type(self):
-            self.assertIs(type(self.amenity.name), str)
-            self.assertFalse(bool(getattr(self.amenity, "name")))
+    def test_amenity_check_attr_type(self):
+        self.assertIs(type(self.amenity.name), str)
+        self.assertFalse(bool(getattr(self.amenity, "name")))
